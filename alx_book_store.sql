dc700b613c9a87b2826873ecc17c4f4976693c17
pakpang14 <ibrahim.ammed90@gmail.com>
2024-10-28 18:35:09 +0000

    order details table amended

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -28,7 +28,7 @@ CREATE TABLE Order_Details (
     order_detail INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
     book_id, INT,
-    quantity_id INT,
+    quantity DOUBLE NOT NULL,
     FOREIGN KEY (order_id) REFERENCES Orders(order_id),
     FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
